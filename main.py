7fc6dff31cd2ab203ec6ed988c9e9ea231d1ab59
Bulat-Akhmetov <akhmetov.bulat@gmail.com>
2021-09-30 17:09:08 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+**Шаг 1**
+
+Создайте Python-проект со следующей структурой:
+
+- static/
+    - index.css
+    - user.css
+    - search.css
+    - add_user.css
+- templates/
+    - index.html
+    - user.html
+    - search.html
+    - add_user.html
+- app.py
+
+**Шаг 2**
+
+Рядом с файлом `app.py` создайте файл `users.json` и положите туда массив с пользователями:
